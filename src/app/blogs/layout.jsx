33cf7be13eefe5e2428220b3c54d68397901019d
/\/\@SoOd <42731850+masoodMEH@users.noreply.github.com>
2024-12-22 23:53:46 +0330

    implementing nested layouts,favicon,metadata

---
+++ src/app/blogs/layout.jsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: "بلاگ ها",
+};
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <h1 className="text-lg mb-12 font-bold">لیست بلاگ ها</h1>
+      <div className="grid grid-cols-12 gap-8">
+        <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-secondary-500 space-y-4">
+          لیست دسته بندی ها
+        </div>
+        <div className="col-span-12 lg:col-span-4 xl:col-span-9">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/app/blogs/page.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "بلاگ ها",
+};
+
+const Blogs = () => {
+  return <div>blogs lists</div>;
+};
+
+export default Blogs;
